28b64bd1b3b30358b14a431f669ca1e8ee9452fe
milostrifunovic1 <milos.trifunovic.92@gmail.com>
2019-06-12 22:38:36 +0200

    Updates

---
+++ precache-manifest.1e2c59f7f45908c2aa4704580a2ab869.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21afde4afc018aaa72e94f004375a4bf",
+    "url": "/testreact/index.html"
+  },
+  {
+    "revision": "0a5b74a988bfd56cfe83",
+    "url": "/testreact/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/testreact/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "0a5b74a988bfd56cfe83",
+    "url": "/testreact/static/js/main.9580759f.chunk.js"
+  },
+  {
+    "revision": "1d0bc4833f5a10c60360",
+    "url": "/testreact/static/js/runtime~main.30e0e851.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
